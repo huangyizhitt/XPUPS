d9e6f828bdb74a10a4a2d897b1262d14148a2fc7
huangyizhi <huangyizhi@hnu.edu.cn>
2020-09-09 16:26:27 +0800

    [who] huangyizhi

--- src/mfworker.cpp
+++ src/mfworker.cpp
@@ -195,23 +195,25 @@ void MFWorker::PullPushFeature()
 	keys.push_back(1);
 	lens.push_back(size_p);
 	lens.push_back(size_q);
-	
-#ifdef CAL_PORTION_RMSE
-	std::vector<float> vals(p, p+size_p+size_q+1);
-	keys.push_back(2);
-	lens.push_back(1);
-	vals[size_p+size_q] =  std::accumulate(loss.begin(), loss.end(), 0.0);
-#else
-	std::vector<float> vals(p, p+size_p+size_q);
-#endif
 
 	//Pull
-	if(current_epoch == 0) {		
+	if(current_epoch == 0) {	
+		std::vector<float> vals;
 		kv_xpu->Wait(kv_xpu->Pull(keys, &vals, &lens, cmd));
+		memcpy(p, &vals[0], sizeof(float) * size_p);
+		memcpy(q, &vals[size_p], sizeof(float) * size_q);
 	}
 	
 	//Push feature and pull new feature
-	if(current_epoch > 0) {			
+	if(current_epoch > 0) {	
+#ifdef CAL_PORTION_RMSE
+		std::vector<float> vals(p, p+size_p+size_q+1);
+		keys.push_back(2);
+		lens.push_back(1);
+		vals[size_p+size_q] =  std::accumulate(loss.begin(), loss.end(), 0.0);
+#else
+		std::vector<float> vals(p, p+size_p+size_q);
+#endif
 		kv_xpu->Wait(kv_xpu->PushPull(keys, vals, &vals, lens, cmd));
 	}
 
