7f117a424ee5890bebfa866fee4686fbabc2ca8f
huangyizhi <huangyizhi@hnu.edu.cn>
2020-09-09 10:45:13 +0800

    [why] complete zmq ipc [who] huangyizhi

--- local.sh
+++ local.sh
@@ -4,7 +4,7 @@ if [ $# -lt 3 ]; then
     echo "usage: $0 num_servers num_workers bin [args..]"
     exit -1;
 fi
-
+export DMLC_PS_VAN_TYPE='zmq_ipc'
 export DMLC_NUM_SERVER=$1
 shift
 export DMLC_NUM_WORKER=$1
