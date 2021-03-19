b10fec29b3cf63c4a9d3e41d16f05875c2397ab2
huangyizhi <huangyizhi@hnu.edu.cn>
2021-03-19 15:30:19 +0800

    [why] modify the number of 4workers_test [who] huangyizhi

--- netflix_4workers_test.sh
+++ netflix_4workers_test.sh
@@ -2,7 +2,7 @@
 
 log_dir="log"
 test -d ${log_dir} || mkdir -p ${log_dir}
-test_times=100
+test_times=20
 file=${log_dir}/$(date "+%Y%m%d%H%M%S")-netflix
 
 for i in $(seq 1 ${test_times})
@@ -17,7 +17,7 @@ done
 
 for i in $(seq 1 ${test_times})
 do
-        ./script/netflix/4workers/6242_6242l_2080_2080s_6.sh ./mf ${file}-6242_6242l_2080_2080s_6.csv
+        ./script/netflix/4workers/6242_6242l_2080_2080s_6_perfect.sh ./mf ${file}-6242_6242l_2080_2080s_6.csv
 done
 
 
