610b01b4536b65e398aeb909add58c798421d04f
huangyizhi <huangyizhi@hnu.edu.cn>
2021-03-22 09:40:17 +0800

    [why] modify init model NaN->0.0 [who] huangyizhi

--- src/mfdata.cpp
+++ src/mfdata.cpp
@@ -295,7 +295,7 @@ void DataManager::InitModel()
                     			feature[i * k + j] = (float)(distribution(generator)*s);
 			} else {
                 		for(size_t j = 0; j < k; j++)
-                    			feature[i * k + j] = std::numeric_limits<float>::quiet_NaN();
+                    			feature[i * k + j] = 0.0;
             		}
         	}
 	};
@@ -335,7 +335,7 @@ void DataManager::InitModelShm(void *shm_buf)
                     			feature[i * k + j] = (float)(distribution(generator)*s);
 			} else {
                 		for(size_t j = 0; j < k; j++)
-                    			feature[i * k + j] = std::numeric_limits<float>::quiet_NaN();
+                    			feature[i * k + j] = 0.0;
             		}
         	}
 	};
